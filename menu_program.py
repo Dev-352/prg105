4aee6257f8c1643cc818ddf85116b87c24e5c810
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-06-10 16:18:38 -0500

    Add files via upload

---
+++ menu_program.py
@@ -0,0 +1,5 @@
+# Create a program that presents the user with five choices. The topic could be game characters, food, car packages,
+# anything you are interested in. You will put a menu on the screen, ask the user to enter the number or letter
+# of their choice, and then call the corresponding function to give the user more information.
+
+
