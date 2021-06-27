13192176dc8a549aa19b36ec0af684dfce65c880
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-06-27 17:43:19 -0500

    Add files via upload

---
+++ practice 7.1.py
@@ -0,0 +1,9 @@
+# Ask the user how many students are in their class. Get the student's name and final grade. 
+# Store the answers in a two-dimensional list, then write the list to the grades.txt file. 
+
+
+def main():
+    num_students = input("How many srudents do you need to enter grades for? ")
+
+
+main()
