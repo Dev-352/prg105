d7cccb6b80204d6878d0608456d00eb885c1f9c4
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-06-27 18:44:34 -0500

    Delete practice 7.2.py

--- practice 7.2.py
+++
@@ -1,4 +0,0 @@
-# Using parallel arrays, create a secret code creator.
-# Ask the user for text to enter, convert it to the code and write the code to a text file.
-# Include punctuation (. , !), space, upper, and lower case letters.
-
