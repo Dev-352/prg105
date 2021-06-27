bfea8667c3dc1458dbfd002f1a3399bcbde30c58
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-06-27 18:16:04 -0500

    Add files via upload

---
+++ practice 7.2.py
@@ -0,0 +1,4 @@
+# Using parallel arrays, create a secret code creator.
+# Ask the user for text to enter, convert it to the code and write the code to a text file.
+# Include punctuation (. , !), space, upper, and lower case letters.
+
