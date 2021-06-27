50b80ad7cf976ac36e4dab6e114adc8f9b305cae
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-06-27 17:59:28 -0500

    Delete practice 7.1.py

--- practice 7.1.py
+++
@@ -1,9 +0,0 @@
-# Ask the user how many students are in their class. Get the student's name and final grade. 
-# Store the answers in a two-dimensional list, then write the list to the grades.txt file. 
-
-
-def main():
-    num_students = input("How many srudents do you need to enter grades for? ")
-
-
-main()
