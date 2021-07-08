92a211be4773e0ecdf8a411fb8436b31c24cd555
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-07-08 17:20:51 -0500

    Delete practice 9.2.py

--- chapter 9/practice 9.2.py
+++
@@ -1,8 +0,0 @@
-# Create a dictionary based on the language of your choice with the numbers from 1-10 paired with the numbers
-# from 1-10 in English. Create a quiz based on this dictionary. Display the number in a foreign language and
-# ask for the number in English. Score the test and give the user a letter grade.
-
-def main():
-    
-
-main()
