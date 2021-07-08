c0fd1e81004d25c39e1a4ed5292c53730ca4b89c
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-07-08 16:38:31 -0500

    Add files via upload

---
+++ chapter 9/practice9.1.py
@@ -0,0 +1,11 @@
+# You will be tracking the number of steps someone takes each day for a week. Using a loop,
+# ask them to enter the date and the number of steps. At the end of the program,
+# you will display the total number of steps taken, the day with the most steps, and the day with the least steps.
+# Print multiple days if they are tied.
+
+
+def main():
+
+    
+
+main()
