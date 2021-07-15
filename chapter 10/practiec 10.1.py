5456f40069d30e09bda0a464399d8c0cab94c017
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-07-15 11:06:56 -0500

    Delete practiec 10.1.py

--- chapter 10/practiec 10.1.py
+++
@@ -1,6 +0,0 @@
-# Design a class that holds the following personal data: name, address, age, and phone number.
-# Write appropriate accessor and mutator methods (get and set). Write a program that creates three
-# instances of the class. One instance should hold your information and the other two should hold your friends'
-# or family members' information.  Just add information, don't get it from the user.  Print the data from each object,
-# make sure to format the output for clarity and ease of reading.
-
