e70a859764e03aaf91a5e234d12d5fb35e8a8bfb
Dev-352 <84253578+Dev-352@users.noreply.github.com>
2021-07-16 11:17:20 -0500

    Add files via upload

--- chapter 11/ch_11_exercises.py
+++ chapter 11/ch_11_exercises.py
@@ -48,27 +48,108 @@ class Dwelling:
 # -- Call the __init__ of superclass Dwelling and pass the required arguments, remember to include self
 # -- Initialize the garage_type and yard_size attributes
 class SingleFamilyHome(Dwelling):
-    
-# 5) Create the mutator and accessor methods for the garage_type and yard_size attributes
+    def __init__(self, number_of_rooms, square_feet, floors, garage_type, yard_size):
+        Dwelling.__init__(self, number_of_rooms, square_feet, floors)
+        self.__garage_type = garage_type
+        self.__yard_size = yard_size
+
+    # 5) Create the mutator and accessor methods for the garage_type and yard_size attributes
+    def set_garage_type(self, garage_type):
+        self.__garage_type = garage_type
+
+    def set_yard_size(self, yard_self):
+        self.__yard_size = yard_self
+
+    def get_garage_type(self):
+        return self.__garage_type
+
+    def get_yard_self(self):
+        return self.__yard_size
 
 
 # Demonstrate the SingleFamilyHome class, no need to import because you are in the same file
 # 6) Create a main function.
-# 7) In main, create an object from the Single_family_home class with the following information:
-#            6 rooms, 1200 square feet, 1 floor, single car garage, .25 acres
 
-# 8) Display the data using the accessor methods
+
+def main():
+    # 7) In main, create an object from the Single_family_home class with the following information:
+    #            6 rooms, 1200 square feet, 1 floor, single car garage, .25 acres
+
+    home = SingleFamilyHome("6", '1200', '1', "single ", ".25 acres")
+    # 8) Display the data using the accessor methods
+    print("Number of rooms: " + home.get_number_of_rooms() + "\nSquare feet: " + home.get_square_feet() +
+          "\nNumber of floors: " + home.get_floors() + "\nGarage type: " + home.get_garage_type() +
+          "\nYard size: ")
+
 
 # 9) Call the main function
 
 
+main()
 # TODO 11.2 Polymorphism
 print("=" * 10, "Section 11.2 polymorphism", "=" * 10)
+
+
 # 1) Type in the mammal class from program 11-9, lines 1 - 22
+class Mammal:
+    def __init__(self, species):
+        self.__species = species
+
+    def show_species(self):
+        print("I am a ", self.__species)
+
+    def make_sound(self):
+        print("Grrrrr")
+
 
 # 2) Create a Mouse class as a sub class of the mammal class following the Dog example
+class Mousm(Mammal):
+    def __int__(self):
+        Mammal.__init__(self, 'Mouse')
+
+    def make_sound(self):
+        print('Squeek')
+
+
+class Dog(Mammal):
+    def __init__(self):
+        Mammal.__init__(self, "Mouse")
+
+    def make_sound(self):
+        print("Squuek")
+
 
 # 3) Create an Sheep class as a sub class of the mammal class following the Cat Example
+class Sheep(Mammal):
+    def __init__(self):
+        Mammal.__init__(self, "Sheep")
+
+    def make_sound(self):
+        print("baa")
 
 # 4) Follow the example in program 11-10 (no need to import, use main2 instead of main
 #    because there is already a main on this page) use the Mouse and Sheep class that you created
+
+
+def main2():
+    mammal = Mammal("regular animal")
+    sheep = Sheep()
+    dog = Dog()
+
+    print("Here are some animals and")
+    print("The sounds they make.")
+    print("----------------------------")
+    info(mammal)
+    print()
+    info(dog)
+    print()
+    info(sheep)
+    print()
+
+
+def info(creature):
+    creature.show_species()
+    creature.make_sound()
+
+
+main2()
